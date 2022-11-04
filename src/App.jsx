00e791d655e3e66203dc827c49de2102f6fa6dcd
5xDiego <100784396+5xDiego@users.noreply.github.com>
2022-11-04 11:26:11 +0100

    added scroll icon with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Container, Typography } from "@mui/material"
+import { Container, Typography } from "@mui/material";
+import Scrollicon from "./Scroll-Icon";
 
 function App() {
 
   return (
-    <Container>
+    <Container sx={{height:"100%"}}>
       <Typography variant="h1" align="center">Brotverteiler</Typography>
+      <Scrollicon/>
     </Container>
   )
 }
