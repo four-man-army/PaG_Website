7c8addc77f79babdd540372dfe0ddeac2cf08f0a
5xDiego <100784396+5xDiego@users.noreply.github.com>
2022-11-06 01:47:42 +0100

    changed File names

--- src/Scroll-Icon.jsx
+++ src/Scroll-Icon.jsx
@@ -1,14 +1,19 @@
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import { Typography, Box } from '@mui/material';
-import "./floatAnimation.css";
+import { HideOn } from 'react-hide-on-scroll'
+import "./Scroll-Icon.css";
 
-export default function Scrollicon(){
-    return(
-        <Box className='floating' sx={{position:"fixed", bottom:"35px", left:0, right:0}}>
-        <Typography variant="h5" align="center"> scroll </Typography>
-            <Box sx={{width:"100%", display:"flex", justifyContent:"center"}}>
-                <KeyboardDoubleArrowDownIcon />
+
+export default function Scrollicon() {
+
+    return (
+        <HideOn atHeight height={100}>  
+            <Box className='floating' sx={{ position: "fixed", bottom: "35px", left: 0, right: 0 }}>
+                <Typography variant="h5" align="center"> scroll </Typography>
+                <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                    <KeyboardDoubleArrowDownIcon />
+                </Box>
             </Box>
-        </Box>
+        </HideOn>
     )
 }
