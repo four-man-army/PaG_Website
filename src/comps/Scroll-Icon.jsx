cb8b03e467047916a9df1f6456a4709bb7d24bca
Oliver Oswald <oliver.oswald25@gmail.com>
2022-11-06 14:53:04 +0100

    removed hide on scroll

--- src/comps/Scroll-Icon.jsx
+++ src/comps/Scroll-Icon.jsx
@@ -1,19 +1,20 @@
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
-import { Typography, Box } from '@mui/material';
-import { HideOn } from 'react-hide-on-scroll'
+import { Typography, Box } from "@mui/material";
 import "./Scroll-Icon.css";
 
-
 export default function Scrollicon() {
-
-    return (
-        <HideOn atHeight height={100}>  
-            <Box className='floating' sx={{ position: "fixed", bottom: "35px", left: 0, right: 0 }}>
-                <Typography variant="h5" align="center"> scroll </Typography>
-                <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                    <KeyboardDoubleArrowDownIcon />
-                </Box>
-            </Box>
-        </HideOn>
-    )
+  return (
+    <Box
+      className="floating"
+      sx={{ position: "fixed", bottom: "35px", left: 0, right: 0 }}
+    >
+      <Typography variant="h5" align="center">
+        {" "}
+        scroll{" "}
+      </Typography>
+      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <KeyboardDoubleArrowDownIcon />
+      </Box>
+    </Box>
+  );
 }
