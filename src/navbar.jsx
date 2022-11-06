bc2cb83cfd5b9868c272f827105f2ee4c6f8ede7
Oliver Oswald <oliver.oswald25@gmail.com>
2022-11-06 13:00:02 +0100

    fixed animation

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -9,39 +9,29 @@ const Navbar = () => {
       setValue(newValue);
     };
 
-    function HideOnScroll(props) {
-      const { children, window } = props;
-      // Note that you normally won't need to set the window ref as useScrollTrigger
-      // will default to window.
-      // This is only being set here because the demo is in an iframe.
-      const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-      });
-
-      return (
-        <Slide appear={false} direction="down" in={!trigger}>
-          {children}
-        </Slide>
-      );
-    }
-
+    const trigger = useScrollTrigger();
 
     return (
-        <HideOnScroll>
-        <Box sx={{ borderColor: "divider", width: "fit-content", right: 0, p:3, position:"fixed" }}>
-            <Paper elevation={6}>
-            <Tabs
-                onChange={handleChange}
-                value={value}
-            >
-                <Tab value="one" label="Kartell" />
-                <Tab value="two" label="Staat" />
-                <Tab value="three" label="Auswirkungen" />
+      <Slide appear={false} direction="down" in={!trigger}>
+        <Box
+          sx={{
+            borderColor: "divider",
+            width: "fit-content",
+            right: 0,
+            p: 3,
+            position: "fixed",
+          }}
+        >
+          <Paper elevation={6}>
+            <Tabs onChange={handleChange} value={value}>
+              <Tab value="one" label="Kartell" />
+              <Tab value="two" label="Staat" />
+              <Tab value="three" label="Auswirkungen" />
             </Tabs>
-            </Paper>
-            </Box>
-        </HideOnScroll>
-     );
+          </Paper>
+        </Box>
+      </Slide>
+    );
 }
  
 export default Navbar;
