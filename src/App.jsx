17370db2dde163c0d6cb0ea715b8d4debfdd8cfa
5xDiego <100784396+5xDiego@users.noreply.github.com>
2022-11-06 14:58:03 +0100

    work on Page1.jsx, added App.css, imported App.css on App.jsx, downloaded and imported react-intersection-observer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { Container, Typography } from "@mui/material";
+import { useInView } from 'react-intersection-observer'; 
 import Scrollicon from "./comps/Scroll-Icon";
 import Navbar from "./comps/navbar";
 import Page1 from "./pages/Page1";
-
+import "./App.css";
 
 function App() {
+  const {ref: p1ref , inVew: p1IsVisible} = useInView();
+  console.log('p1IsVisible', p1IsVisible); 
 
   return (
     <>
       <Navbar />
-      <Container sx={{ height: "110%", pt: 50}}>
+      <Container sx={{ height: "120%", pt: 50 }}>
         <Typography variant="h1">
           Eine Reise durch den mexikanischen Drogenkrieg
         </Typography>
         <Scrollicon />
       </Container>
-      <Page1/>
+      <section ref={p1ref} >
+        <Page1 />
+      </section>
     </>
   );
 }
 
-export default App
+export default App;
