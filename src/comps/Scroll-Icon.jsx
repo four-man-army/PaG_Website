a26c9f7bb0a1b2f39beeef7eb130e253fa76de7d
5xDiego <100784396+5xDiego@users.noreply.github.com>
2022-11-07 16:33:36 +0100

    hotfix; added HideOn to Scroll-icon.jsx

--- src/comps/Scroll-Icon.jsx
+++ src/comps/Scroll-Icon.jsx
@@ -1,20 +1,23 @@
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import { Typography, Box } from "@mui/material";
+import { HideOn } from "react-hide-on-scroll";
 import "./Scroll-Icon.css";
 
 export default function Scrollicon() {
   return (
-    <Box
-      className="floating"
-      sx={{ position: "fixed", bottom: "35px", left: 0, right: 0 }}
-    >
-      <Typography variant="h5" align="center" color="rgb(21, 21, 21)">
-        {" "}
-        scroll{" "}
-      </Typography>
-      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
-        <KeyboardDoubleArrowDownIcon/>
+    <HideOn atHeight height={100}>
+      <Box
+        className="floating"
+        sx={{ position: "fixed", bottom: "35px", left: 0, right: 0 }}
+      >
+        <Typography variant="h5" align="center" color="rgb(21, 21, 21)">
+          {" "}
+          scroll{" "}
+        </Typography>
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <KeyboardDoubleArrowDownIcon />
+        </Box>
       </Box>
-    </Box>
+    </HideOn>
   );
 }
