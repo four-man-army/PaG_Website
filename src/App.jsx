d2a8403291e5cd9535c77b73af700c04506453b5
Oliver Oswald <oliver.oswald25@gmail.com>
2022-11-17 12:30:18 +0100

    added video and theaming

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { InView } from 'react-intersection-observer';
 import Scrollicon from "./comps/Scroll-Icon";
 import Navbar from "./comps/navbar";
 import Page1 from "./pages/Page1";
-import Video from "./comps/video";
+import video from "./assets/video/mexico.webm";
 import "./App.css";
 
 
@@ -11,21 +11,28 @@ function App() {
 
   return (
     <>
-    <Video />
       <Navbar />
+      <div className="video-container">
+      <video autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      </div>
       <Container sx={{ height: "110vh", pt: 50 }}>
         <Typography variant="h1">
           Eine Reise durch den mexikanischen Drogenkrieg
         </Typography>
         <Scrollicon />
       </Container>
-      <InView as="section" onChange={(inView, entry) => {
-        if (inView) {
-          // Scroll to the Page1 component
-          entry.target.scrollTo(0, 0);
-          console.log("Page1");
-        }
-      }}>
+      <InView
+        as="section"
+        onChange={(inView, entry) => {
+          if (inView) {
+            // Scroll to the Page1 component
+            entry.target.scrollTo(0, 0);
+            console.log("Page1");
+          }
+        }}
+      >
         <Page1 />
       </InView>
     </>
