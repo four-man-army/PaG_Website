1dbc477d6a80a46dd9e91fdb061cf0036ba733d2
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-24 09:26:34 +0100

    fixed the problem

--- src/comps/interactivtank.jsx
+++ src/comps/interactivtank.jsx
@@ -1,64 +1,84 @@
-import { Button,  } from "@mui/material";
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import { useState } from "react";
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 import "./interactivtank.css";
 import { Container } from "@mui/system";
 
 //--------IMG-----------
-import Tankback from '../assets/img/Tank/tank2.png';
-import Turretlayer from '../assets/img/Tank/tabbnk.png';
-import Wheelplate from '../assets/img/Tank/wheel.png';
-import Front from '../assets/img/Tank/front.png';
-import Window from '../assets/img/Tank/window.png';
-import Shootingslid from '../assets/img/Tank/shootingslid.png';
+import Tankback from "../assets/img/Tank/tank2.png";
+import Turretlayer from "../assets/img/Tank/tabbnk.png";
+import Wheelplate from "../assets/img/Tank/wheel.png";
+import Front from "../assets/img/Tank/front.png";
+import Window from "../assets/img/Tank/window.png";
+import Shootingslid from "../assets/img/Tank/shootingslid.png";
 
-const images = [ {image: Turretlayer, name: "Turret"}, {image: Wheelplate, name: "Armor"}, {image: Front, name: "Bumper"}, {image: Window, name: "Window"}, {image: Shootingslid, name: "Gunslot"}];
-
-
+const images = [
+  { image: Turretlayer, name: "Turret", ref: null },
+  { image: Wheelplate, name: "Armor", ref: null },
+  { image: Front, name: "Bumper", ref: null },
+  { image: Window, name: "Window", ref: null },
+  { image: Shootingslid, name: "Gunslot", ref: null },
+];
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
 }));
-  
-  
+
 const Tank = () => {
-    
-    return (
-        <>
-            <div className="background">
-                <img src={Tankback} width="100%" height="100%" />
-                <div>
-                    {
-                        images.map((image, index) => (
-                            <img  className="layers" src={image.image} width="100%" height="100%" />
-                        ))
-                    }
-                </div>
 
-                {
-                    // Button
-                }
+    const resetImg = () => {
+        images.map((image) => {
+            image.ref.style.opacity = 0;
+        })
+    }
 
-            </div>
-            <Container sx={{ display: "flex", gap: 3, justifyContent: "space-between" }}>
-                {
-                    images.map((image, i) => <Button className="tankbutton" key={i} variant="contained">{image.name}</Button>)
-                }
-                
-            </Container>
+  return (
+    <>
+      <div className="background">
+        <img src={Tankback} width="100%" height="100%" />
+        <div>
+          {images.map((image, index) => (
+            <img
+              className="layers"
+              ref={(el) => (image.ref = el)}
+              src={image.image}
+              width="100%"
+              height="100%"
+            />
+          ))}
+        </div>
 
-            {
-                // Text
-            }
+        {
+          // Button
+        }
+      </div>
+      <Container
+        sx={{ display: "flex", gap: 3, justifyContent: "space-between" }}
+      >
+        {images.map((image, i) => (
+          <Button
+            className="tankbutton"
+            key={i}
+            onClick={() => {
+                resetImg();
+                image.ref.style.opacity = 1;
+            }}
+            variant="contained"
+          >
+            {image.name}
+          </Button>
+        ))}
+      </Container>
 
-        
-        </>
-    );
-}
+      {
+        // Text
+      }
+    </>
+  );
+};
 
 export default Tank;
