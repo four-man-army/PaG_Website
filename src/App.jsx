dd3e489129c4c81ef87656d7e72bed1cffdcb41d
Oliver Oswald <oliver.oswald25@gmail.com>
2023-01-18 21:21:48 +0100

    made navbar stay

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Container, Typography } from "@mui/material"
+import Navbar from "./navbar"
 
 function App() {
 
   return (
     <Container>
+      <Navbar />  
       <Typography variant="h1" align="center">Brotverteiler</Typography>
     </Container>
   )
